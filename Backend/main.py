3bc5f58c465a246552e45a9a5f956d0114e63afb
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2025-05-26 22:20:34 -0400

    fixed holdings schema reference to portfolio id

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import yfinance as yf
 import requests
 
+
 app = FastAPI()
 
 app.add_middleware(
@@ -23,3 +24,4 @@ def get_price(symbol: str):
     return {"symbol": symbol, "price": round(float(current_price), 2)}
   except Exception as e:
     return {"error": str(e)}
+
